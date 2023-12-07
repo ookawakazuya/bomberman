262f6f4ed318a07ce2c5be2c8f8a5b7c20ee614c
ookawakazuya <oozoratiga@gmail.com>
2023-12-07 12:13:52 +0900

    動く壁の実装

--- Assets/ookawakazuya/Script/transfomWoll.cs
+++ Assets/ookawakazuya/Script/transfomWoll.cs
@@ -13,6 +13,17 @@ public class transfomWoll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 自分自身のTransformを取得
+        Transform myTransform = this.transform;
+
+        // 先ほど取得したTransformの情報から座標の情報を取得
+        // Vector3：3D ベクトルと位置の表現
+        Vector3 pos = myTransform.position;
+
+        // x,y,z座標の値をフレームごと0.05加算する
+        pos.x -= 0.05f;
+
+        // 自分の座標を加算した値に設定
+        myTransform.position = pos;
     }
 }
