d60226c9af57b966e5671e68078cc70fa2535941
ookawakazuya <oozoratiga@gmail.com>
2023-12-21 10:43:57 +0900

    ゲームステージの追加

---
+++ Assets/ookawakazuya/Script/MoveLeft.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    [SerializeField]private float speed = 30;
+   // private PlayerController playerControllerScript;
+    void Start()
+    {
+        //playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    [SerializeField]private float leftBound = 0;
+    void Update()
+    {
+        //if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if(transform.position.x< leftBound && gameObject.CompareTag("DestroyWall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //壊れないブロックや壊されなかったブロックを消す
+    private void OnTriggerEnter(Collider collider)
+    {
+        //衝突したときに相手にWallタグが付いているときに
+        if(collider.gameObject.tag == "DestroyWall")
+        {   //このオブジェクトを消滅させる
+            Destroy(gameObject,2f);
+        }
+    }
+}
