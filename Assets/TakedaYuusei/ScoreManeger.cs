8370e6f7fdcb98ccc6b06b94d672848469af72af
704-02\cre <a>
2024-01-18 12:30:47 +0900

    ランキングの実装

--- Assets/TakedaYuusei/ScoreManeger.cs
+++ Assets/TakedaYuusei/ScoreManeger.cs
@@ -9,39 +9,61 @@ public class ScoreManager : MonoBehaviour
     public GameObject score_object = null; // Text
     public static int score_num = 0; // スコア変数
     float time;
+    bool alive = true;
+
+
 
     void Start()
     {
         
     }
- 
-    void Update()
+
+    private void AliveScore()
     {
-       time += Time.deltaTime;
+        time += Time.deltaTime;
 
         Text score_text = score_object.GetComponent<Text>();
-        
-        score_text.text = "Score:" + score_num;
-
-        if(time >= 1)
-        {
-            time = 0;
-            score_num += 100;
-        }
 
         /*if(ブロックが壊されたら)//合わせたら書く
         {
             score_num += 300;
         }*/
 
-
         if (score_num >= 500) //collision.gameObject.name == "LimitWall" 合わせるとき条件式を(プレイヤーが死んだらにする)
         {
-            SceneManager.LoadScene("result");
+            RankingManager.instance.AddScore(score_num);
+            Invoke("Deray", 2.0f);
+            alive = false;
+
+        }
+
+        if (time >= 100)//スコア加算
+        {
+            time = 0;
+            score_num += 1;
+        }
+
+        score_text.text = "Score:" + score_num;
+    }
+
+    void Update()
+    {
+
+        if (alive)
+        {
+            AliveScore();
+        }
+        else
+        {
+
         }
 
     }
 
+    private void Deray()
+    {
+        SceneManager.LoadScene("result");//リザルト画面へ移動
+    }
 
     public static int getscore_num()
     {
--- Assets/TakedaYuusei/resultScore.cs
+++ Assets/TakedaYuusei/resultScore.cs
@@ -5,20 +5,28 @@ using UnityEngine.UI;
 
 public class resultScore : MonoBehaviour
 {
-    public Text ScoreText;
+    [SerializeField] Text playerScore;
+    public Text[] ScoreText;
+    Text rankIn;
     int score_num;
 
     // Start is called before the first frame update
     void Start()
     {
         score_num = ScoreManager.getscore_num();
+        for (int i = 0; i < RankingManager.instance.scores.Count; i++)
+        {
+            int score = RankingManager.instance.scores[i];
+            ScoreText[i].text = string.Format("Score:{0}", score);
 
-        ScoreText.text = string.Format("Score:{0}",score_num);
+            if(score == score_num) rankIn = ScoreText[i];//ランクインした場合
+        }
+        playerScore.text = string.Format("Score:{0}",score_num);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //ランクインした場合のアニメーションとか
     }
 }
